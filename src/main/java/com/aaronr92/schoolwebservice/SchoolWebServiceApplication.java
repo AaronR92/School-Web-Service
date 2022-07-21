606909390860b9ba6d3669dd5d6bf35843c42a41
AaronR92 <86726028+AaronR92@users.noreply.github.com>
2022-07-21 10:47:47 +0300

    Security Changes
    Added CustomAuthEntryPoint and basic auth

--- src/main/java/com/aaronr92/schoolwebservice/SchoolWebServiceApplication.java
+++ src/main/java/com/aaronr92/schoolwebservice/SchoolWebServiceApplication.java
@@ -19,9 +19,4 @@ public class SchoolWebServiceApplication {
     BCryptPasswordEncoder getEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
 }
--- src/main/java/com/aaronr92/schoolwebservice/security/WebSecurityConfig.java
+++ src/main/java/com/aaronr92/schoolwebservice/security/WebSecurityConfig.java
@@ -1,23 +1,18 @@
 package com.aaronr92.schoolwebservice.security;
 
-import com.aaronr92.schoolwebservice.filter.CustomAuthenticationFilter;
-import com.aaronr92.schoolwebservice.filter.CustomAuthorizationFilter;
 import com.aaronr92.schoolwebservice.service.UserService;
 import com.aaronr92.schoolwebservice.util.Role;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.springframework.http.HttpMethod.*;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
 
 @Configuration
 @EnableWebSecurity
@@ -30,16 +25,12 @@ public class WebSecurityConfig {
     BCryptPasswordEncoder passwordEncoder;
 
     @Autowired
-    AuthenticationManager authenticationManager;
+    CustomAuthenticationEntryPoint authenticationEntryPoint;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        CustomAuthenticationFilter customAuthenticationFilter =
-                new CustomAuthenticationFilter(authenticationManager);
-        customAuthenticationFilter.setFilterProcessesUrl("/api/login");
-        http.exceptionHandling()
-                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
-        http.httpBasic();
+        http.httpBasic()
+                .authenticationEntryPoint(authenticationEntryPoint);
         http.csrf().disable();
         http.authorizeRequests()
                 .antMatchers("/api/login/**").permitAll()
@@ -48,8 +39,6 @@ public class WebSecurityConfig {
                 .anyRequest().authenticated();
         http.sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.addFilter(customAuthenticationFilter);
-        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
